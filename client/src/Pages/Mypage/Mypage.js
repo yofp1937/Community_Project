0a1865ff47e69c24b9d366de6ce3b2e32cf5e723
yofp1937 <yofp1937@naver.com>
2024-02-17 00:56:05 +0900

    마이페이지 화면 구성만 해놈

--- client/src/Pages/Mypage/Mypage.js
+++ client/src/Pages/Mypage/Mypage.js
@@ -5,12 +5,25 @@ function Mypage() {
   return (
     <div className={styles.mypage}>
         <div className={styles.nick}>
-
+          <span className={styles.span_nick}>닉네임</span>
+          <a href="#">변경</a>
         </div>
 
+        <hr/>
+
         <div className={styles.bar}>
-          <span>작성글</span>
-          <span>작성댓글</span>
+          <span className={styles.post}>작성글</span>
+          <span className={styles.comment}>작성댓글</span>
+        </div>
+
+        <hr/>
+
+        <div className={styles.postlist}>
+          작성글 리스트
+        </div>
+
+        <div className={styles.commentlist}>
+          작성댓글 리스트
         </div>
     </div>
   );
